6ea8d9e629d78c6c8d62924f2ed9a491eecc252a
Syed Ekramul Khayer Sayem <168598535+Syed-Sayem@users.noreply.github.com>
2026-06-29 21:42:32 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const daysE1=document.getElementById("day");
+const hoursE1=document.getElementById("hour");
+const minuteE1=document.getElementById("minute");
+const secondE1=document.getElementById("second");
+
+const newYearTime=new Date("Jan 1,2027 00:00:00").getTime();
+
+updateCountdown();
+
+function updateCountdown(){
+  const now=new Date().getTime();
+  const gap=newYearTime-now;
+  const second=1000;
+  const minute=second*60;
+  const hour=minute*60;
+  const day=hour*24;
+
+  const d=Math.floor(gap/day);
+  const h=Math.floor((gap%day)/hour);
+  const m=Math.floor((gap%hour)/minute);
+  const s=Math.floor((gap%minute)/second);
+  daysE1.innerText=d;
+  hoursE1.innerText=h;
+  minuteE1.innerText=m;
+  secondE1.innerText=s;
+  setTimeout(updateCountdown,1000);
+}
